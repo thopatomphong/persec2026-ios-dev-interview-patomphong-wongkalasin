7530f41525f777d98189acc6a6cef89dbfee1824
Patomphong Wongkalasin <patomphong.tho@gmail.com>
2026-02-04 01:16:27 +0700

    Initial Commit

---
+++ PersecTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PersecTest
+//
+//  Created by Patomphong Wongkalasin on 4/2/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PersecTest/PersecTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersecTestApp.swift
+//  PersecTest
+//
+//  Created by Patomphong Wongkalasin on 4/2/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct PersecTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
